1d221affd62c1422bae88453eb87c2965d686df3
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-03-28 20:48:19 +0300

    test

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
 'use strict';
+//Load envirnment variables from the .env file
 require('dotenv').config();
 // Application dependencies
 const express = require('express');//from node modules
 const cors = require('cors');//from node modules
-const PORT =process.env.PORT;
+let PORT =process.env.PORT;
 const app=express(); //create new instance for express //express is a framework
+app.use(cors());
 
-app.listen(3000,()=>console.log('Listening to Port 3000'));//start point for the application"initialisation"
+app.get('/location',handelLocationReq);
+app.get('/weather',handelWeatherReq);
+function handelLocationReq(req,res){
+const searchQuery = req.query;
+const locationData= require('./data/location.json');
+const location = new Location(locationData[0]);
+res.send(location);
+}
+function Location(data){
+this.display_name = data.display_name;
+this.latitude = data.lat;
+this.longitude = data.lon;
+}
+
+function handelWeatherReq(req,res){
+const searchWeather = req.query; 
+const weatherData=require('./data/weather.json');
+const weatherArr=[];
+weatherData.data.forEach(element => {
+ weatherArr.push(new Weather(element)); 
+});
+res.send(weatherArr);
+}
+function Weather(day){
+this.forecast=day.weather.description;
+this.time=day.datetime;   
+}
+// error message 
+app.get('/*' , function(req , res){
+  res.status(500).send('Sorry, something went wrong');
+});
+
+app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));//start point for the application"initialisation"
+
+
