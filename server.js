26946baedc3f4d1aed8c3048ce4490ba042c64f0
Noura-Alquran <nmalquran15@eng.just.edu.jo>
2021-03-31 19:05:35 +0300

    done

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const pg=require('pg');
 const superagent = require('superagent');
 const cors = require('cors');//from node modules
 const { query } = require('express');
-const PORT =process.env.PORT ||5050 ;
+const PORT =process.env.PORT ;
 const GEOCODE_API_KEY=process.env.GEOCODE_API_KEY;
 const WEATHER_API_KEY=process.env.WEATHER_API_KEY;
 const PARKS_API_KEY=process.env.PARKS_API_KEY;
@@ -22,51 +22,44 @@ app.get('/parks',handelParkReq);
 
 function handelLocationReq(req, res) {
   const city = req.query.city;
-  const url = 'https://us1.locationiq.com/v1/search.php';
-  const cityQueryParam = {
-    key: GEOCODE_API_KEY,
-    q:city,
-    format: 'json',
-  };
-
-  if(!city) {
+ if(!city) {
     res.status(500).send('Sorry, something went wrong');
   }
+  const locationvalue = [city];
+  const sqlQuery = `SELECT * FROM locations WHERE search_query=$1`;
+   return client.query(sqlQuery, locationvalue).then(result => {
+    if(result.rowCount){
+      res.send( result.rows[0]);
+      console.log("from database");
+    }
+    else{    
+    const url = 'https://us1.locationiq.com/v1/search.php';
+    const cityQueryParam = {
+    key: GEOCODE_API_KEY,
+    city  :  city,
+    format: 'json',
+      }
 
-  superagent.get(url).query(cityQueryParam).then(resdata => {
-    const location = new Location(city,resdata.body[0]);
-    res.status(200).send(location);
-  }).catch((error) =>{
-    res.send('Sorry, something went wrong');
-  });
-}
+    superagent.get(url).query(cityQueryParam).then(resdata => {
+           const location = new Location(resdata.body[0],city);
+           const safeValues = [city, resdata.body[0].display_name,resdata.body[0].lat, resdata.body[0].lon];
+          const sqlQuery=`INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4)`;
+           client.query(sqlQuery, safeValues).then(result => {
+           res.send(location);
+           console.log("from API");
+      })
+     });
+  }
+})}
 
-function Location(city, geoData) {
+function Location(geoData,city)  {
   this.search_query = city;
   this.formatted_query = geoData.display_name;
   this.latitude = geoData.lat;
   this.longitude = geoData.lon;
 
 }
-// function handelWeatherReq(req,res){
-// const url='https://api.weatherbit.io/v2.0/forecast/daily'
-// const weatherQueryParam= {
-//   key: WEATHER_API_KEY,
-//   q:city,
-//   format: 'json',
-// };
-// superagent.get(url).query(weatherQueryParam).then(res => {
-// const weatherArr=[];
-// res.body.data.map(element => {
-//  weatherArr.push(new Weather(element))
-// return weatherArr;
-// console.log(weatherArr);
-// });
-// res.send(weatherArr); 
-// }).catch((error) =>{
-//   res.send('Sorry, something went wrong');
-// });
-// }
+
 function handelWeatherReq(req,res){
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${req.query.latitude}&lon=${req.query.longitude}&key=${WEATHER_API_KEY}`;
     superagent.get(url).then(resData=> {
@@ -113,7 +106,8 @@ app.use('*' , function(req , res){
 
 client.connect().then(() => {
   app.listen(PORT, () => {
-    console.log("Connected to database:", Client.connectionParameters.database) //show what database we connected to
+    console.log("Connected to database:", client.connectionParameters.database) //show what database we connected to
     console.log(`Listening to Port ${PORT}`);//start point for the application"initialisation"
   });
 })
+
